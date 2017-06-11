4c8c76737c02d7c183fcf4e2afaac649c17dc135
grj <grj@yy.com>
2017-06-11 17:33:19 +0800

    add mp3s

--- js/meta.js
+++ js/meta.js
@@ -125,10 +125,6 @@ var metaData =
             "mp3/vegetable/cookie_i_have.mp3", 
             "mp3/vegetable/cookie_is.mp3"
         ], 
-        "orange": [
-            "mp3/vegetable/orange.mp3", 
-            "mp3/vegetable/orange_is.mp3"
-        ], 
         "watermelon": [
             "mp3/vegetable/watermelon.mp3", 
             "mp3/vegetable/watermelon_i_have.mp3", 
@@ -138,6 +134,92 @@ var metaData =
             "mp3/vegetable/milk.mp3", 
             "mp3/vegetable/milk_i_have.mp3", 
             "mp3/vegetable/milk_is.mp3"
+        ]
+    }, 
+    "color": {
+        "blue": [
+            "mp3/color/blue.mp3"
+        ], 
+        "brown": [
+            "mp3/color/brown.mp3", 
+            "mp3/color/brown_is.mp3"
+        ], 
+        "purple": [
+            "mp3/color/purple.mp3", 
+            "mp3/color/purple_is.mp3"
+        ], 
+        "pink": [
+            "mp3/color/pink.mp3", 
+            "mp3/color/pink_is.mp3"
+        ], 
+        "green": [
+            "mp3/color/green.mp3", 
+            "mp3/color/green_is.mp3"
+        ], 
+        "orange": [
+            "mp3/color/orange.mp3", 
+            "mp3/color/orange_is.mp3"
+        ]
+    }, 
+    "new": {
+        "heart": [
+            "mp3/new/heart.mp3", 
+            "mp3/new/heart_is.mp3"
+        ], 
+        "square": [
+            "mp3/new/square.mp3"
+        ], 
+        "star": [
+            "mp3/new/star.mp3", 
+            "mp3/new/star_is.mp3"
+        ], 
+        "kite": [
+            "mp3/new/kite.mp3", 
+            "mp3/new/kite_is.mp3"
+        ], 
+        "family": [
+            "mp3/new/family.mp3"
+        ], 
+        "doll": [
+            "mp3/new/doll.mp3", 
+            "mp3/new/doll_i_want.mp3", 
+            "mp3/new/doll_is.mp3"
+        ], 
+        "car": [
+            "mp3/new/car.mp3", 
+            "mp3/new/car_i_want.mp3", 
+            "mp3/new/car_is.mp3"
+        ], 
+        "anthill": [
+            "mp3/new/anthill.mp3"
+        ], 
+        "boll": [
+            "mp3/new/boll.mp3", 
+            "mp3/new/boll_i_want.mp3", 
+            "mp3/new/boll_is.mp3"
+        ], 
+        "truck": [
+            "mp3/new/truck.mp3", 
+            "mp3/new/truck_i_want.mp3", 
+            "mp3/new/truck_is.mp3"
+        ], 
+        "rectangle": [
+            "mp3/new/rectangle.mp3", 
+            "mp3/new/rectangle_is.mp3"
+        ], 
+        "triangle": [
+            "mp3/new/triangle.mp3"
+        ], 
+        "airplane": [
+            "mp3/new/airplane.mp3", 
+            "mp3/new/airplane_i_want.mp3", 
+            "mp3/new/airplane_is.mp3"
+        ], 
+        "circle": [
+            "mp3/new/circle.mp3"
+        ], 
+        "wind": [
+            "mp3/new/wind.mp3"
         ]
     }, 
 };
