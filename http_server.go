b79aba1ca72ba5bd4995d68b0cc29f634fc83797
grj <grj@yy.com>
2017-07-20 16:43:10 +0800

    http

--- http_server.go
+++ http_server.go
@@ -6,5 +6,6 @@ import (
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("./"))))
+	http.Handle("/learn/", http.StripPrefix("/learn/", http.FileServer(http.Dir("."))))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
